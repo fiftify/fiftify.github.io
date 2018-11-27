7b309422d2b73dcfaa390b5fd697c6a849ad3ae6
Shubham Tiwari <websbiz0000@gmail.com>
2018-11-27 14:46:58 +0000

    updates

---
+++ precache-manifest.eddf29450a6f7b4702ed009aec0a39e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb7814be95e09cd2c9a5",
+    "url": "/static/js/main.cb7814be.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50efc2d5eb6d02a457c26626171fb3df",
+    "url": "/index.html"
+  }
+];
